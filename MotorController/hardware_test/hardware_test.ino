3b47e9ff1034daef925e7e40b5959515913e701e
Evan <evan@evanw.org>
2015-02-09 21:20:23 -0500

    led blinks fault codes

--- MotorController/hardware_test/hardware_test.ino
+++ MotorController/hardware_test/hardware_test.ino
@@ -15,29 +15,42 @@ analogWrite(PWM_PIN,0);
 digitalWrite(DIR_PIN,LOW);
 pinMode(LED_PIN,OUTPUT);
 
-
 pinMode(FAULT1_PIN,OUTPUT);
-//digitalWrite(FAULT1_PIN,HIGH);
+digitalWrite(FAULT1_PIN,HIGH);
 pinMode(FAULT2_PIN,OUTPUT);
-//digitalWrite(FAULT2_PIN,HIGH);
+digitalWrite(FAULT2_PIN,HIGH);
 digitalWrite(RST_PIN,HIGH);
 
 }
 
+
+
 void loop(){
-  digitalWrite(LED_PIN,HIGH);
+  if(digitalRead(FAULT1_PIN)){
+    for(int i=0;i<5;i++){
+    digitalWrite(LED_PIN,HIGH);
+    delay(100);
+    digitalWrite(LED_PIN,LOW);
+    delay(100);
+    }
+  }
+  if(digitalRead(FAULT2_PIN)){
+    for(int i=0;i<2;i++){
+    digitalWrite(LED_PIN,HIGH);
+    delay(500);
+    digitalWrite(LED_PIN,LOW);
+    delay(500);
+    }
+  }
   digitalWrite(DIR_PIN,LOW);
   analogWrite(PWM_PIN,100);
-  delay(1000);
+  delay(500);
   analogWrite(PWM_PIN,0);
-  delay(1000);
-  digitalWrite(LED_PIN,LOW);
-
+  delay(500);
   digitalWrite(DIR_PIN,HIGH);
   analogWrite(PWM_PIN,100);
-  delay(1000);
+  delay(500);
   analogWrite(PWM_PIN,0);
-  delay(1000);
+  delay(500);
 
 }
-
