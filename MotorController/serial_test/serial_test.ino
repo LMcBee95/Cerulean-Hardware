fd6b6fdd265811e2a4068a02ddca6880f63cfe65
rmcbee <rmcbee@purdue.edu>
2015-02-20 16:40:21 -0500

    Created test code for the arduino to simulate the top board

---
+++ MotorController/serial_test/serial_test.ino
@@ -0,0 +1,68 @@
+uint8_t topPacket[16];
+
+
+uint8_t checksum(uint8_t* packet, uint8_t start_index, uint8_t size) {
+	uint8_t crc = 0;
+	*packet += start_index;
+	for (uint8_t i = 1; i < 14; i++) {
+		uint8_t inbyte = packet[i];
+		for (uint8_t i = 8; i; i--) {
+			uint8_t mix = (crc ^ inbyte) & 0x01;
+			crc >>= 1;
+			if (mix)
+				crc ^= 0xD5;
+			inbyte >>= 1;
+		}
+	}
+	return crc;
+}
+
+
+
+void setup()
+{
+	
+	
+	//Puts mock data into the packet
+	
+	topPacket[1] = 0;
+	topPacket[2] = 0;
+	topPacket[3] = 0;
+	topPacket[4] = 0;
+	topPacket[5] = 0;
+	topPacket[6] = 50;
+	topPacket[7] = 0;
+	topPacket[8] = 0;
+	topPacket[9] = 9;
+	topPacket[10] = 10;
+	topPacket[11] = 11;
+	topPacket[12] = 12;
+	topPacket[13] = 13;
+	topPacket[14] = checksum(topPacket, 1, 13);
+	topPacket[15] = 0x13;
+	delay(10);
+	topPacket[0] = 0x12;
+
+	
+	uint8_t state = 0;
+	
+	uint16_t timer = 0;
+
+        Serial.begin(19200);
+}
+
+void sendPacket(void)
+{
+	for(uint8_t i = 0; i < 16; i++)
+	{
+	  Serial.write(topPacket[i]);
+	}
+}
+	
+	void loop()
+	{
+			sendPacket();
+		        delay(200);
+		
+	}
+
