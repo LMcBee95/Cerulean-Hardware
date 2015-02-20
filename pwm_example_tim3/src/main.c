184a04c1d87bb73a0a375fbaf76fca617ec82ea3
rmcbee <rmcbee@purdue.edu>
2015-02-20 07:25:46 -0500

    PWM functions are here!!!!!!!!!!!!!!!

--- pwm_example_tim3/src/main.c
+++ pwm_example_tim3/src/main.c
@@ -11,9 +11,15 @@
 #include <stm32f4xx_tim.h>  //library that contains all of the code the makes the pwm work
 
 
-//This function initializes the pins that are used in pwm, assigns them to their alternate functions, and then initializes the TIM(ers) for
-//those pins. 
-uint16_t initialize_timers(void)
+/*This function initializes the pins that are used in pwm, assigns them to their alternate functions, 
+ *and then initializes the TIM(ers) for those pins. For parameters, it takes the frequency of the pwm 
+ *and the prescaler for the clock. The frequency will work fine for anything bellow 525000, but it is 
+ *not guaranteed to work above that. The prescaler divides into the stm boards own internal clock to 
+ *get a clock speed for the timer. The prescaler works good at 1, but it can take any multiple of two 
+ *as its input. The function will return the period of the pwm which is used to calculate the duty cycle 
+ *when setting the pwm for each pin 
+ */
+uint16_t initialize_timers(uint16_t frequency, uint16_t preScaler)
 {
 	// Enable TIM3 and GPIOC clocks
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
@@ -36,9 +42,9 @@ uint16_t initialize_timers(void)
 	GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_TIM3);
 	 
 	// Compute prescaler value for timebase
-	uint16_t PrescalerValue = (uint16_t) ((SystemCoreClock /2) / 84000000) - 1;  //To figure out what the numbers do
+	uint16_t PrescalerValue = (uint16_t) ((SystemCoreClock /2) / (84000000 * preScaler)) - 1;  //To figure out what the numbers do
 	//second value in the divide is the frequency
-	uint16_t PreCalPeriod = (84000000 / 525000) - 1;  //To figure out what the numbers do
+	uint16_t PreCalPeriod = ((84000000 * preScaler) / frequency) - 1;  //To figure out what the numbers do
 
 	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;  //structure used by stm in initializing the pwm
     TIM_OCInitTypeDef  TIM_OCInitStructure;
@@ -75,14 +81,25 @@ uint16_t initialize_timers(void)
 	return(PreCalPeriod);
 }
 
+/*
+ *This function controls the voltage outputted by each pwm pin. For parameters, it takes the TIM(er) of the 
+ *pin that is being changed, which CCRx (Constant current reduction) should be selected, the period of the duty 
+ *cycle (which can be obtained from the initialization function), and the desired duty cycle (between 0 and 255).
+ */
+
+void anologWrite(uint16_t TIMx, uint16_t CCRx, uint16_t period, uint8_t dutyCycle)
+{
+	TIMx->CCRx = (period + 1) * dutyCycle / 255.0;
+}
+
 
 int main(void)
 {
-	uint16_t period = initialize_timers();
+	period = uint16_t period = initialize_timers(525000, 1);
 	
 	while(1)
 	{
-			TIM3->CCR1 = (period + 1) * 50 / 100; //Sets the dury cycle of the pulse width modulation. The duty cycle is how long the pin stay on during a cycle.
+			TIM3->CCR1 = (period + 1) * 50.0 / 255.0; //Sets the dury cycle of the pulse width modulation. The duty cycle is how long the pin stay on during a cycle.
 							 //So the higher the duty cycle, up until the max duty cycle, the higher the voltage the pulse width will emit.
 			//sets the duty cycle of the other pins
 			TIM3->CCR2 = 40; 
