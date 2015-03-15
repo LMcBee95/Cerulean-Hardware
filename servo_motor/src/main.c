b00ee8435a30aab60b62bc550286d09e0d2b085a
rmcbee <rmcbee@purdue.edu>
2015-03-15 09:24:14 -0400

    Servo code!!!!!

--- servo_motor/src/main.c
+++ servo_motor/src/main.c
@@ -11,6 +11,72 @@
 #include <misc.h>			 
 #include <stm32f4xx_tim.h>  //library that contains all of the code the makes the pwm work
 
+#define MAXSERVO 2.1
+#define MINSERVO 0.8
+#define MAXSERVOANGLE 135.0
+
+
+void init_USART1(uint32_t baudrate){
+        
+        /* This is a concept that has to do with the libraries provided by ST
+         * to make development easier the have made up something similar to 
+         * classes, called TypeDefs, which actually just define the common
+         * parameters that every peripheral needs to work correctly
+         * 
+         * They make our life easier because we don't have to mess around with 
+         * the low level stuff of setting bits in the correct registers
+         */
+        GPIO_InitTypeDef GPIO_InitStruct;   // this is for the GPIO pins used as TX and RX
+        USART_InitTypeDef USART_InitStruct; // this is for the USART1 initialization
+        
+        /* enable APB2 peripheral clock for USART1 
+         * note that only USART1 and USART6 are connected to APB2
+         * the other USARTs are connected to APB1
+         */
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+        
+        /* enable the peripheral clock for the pins used by 
+         * USART1, PB6 for TX and PB7 for RX
+         */
+        RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+        
+        /* This sequence sets up the TX and RX pins 
+         * so they work correctly with the USART1 peripheral
+         */
+        GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7; // Pins 6 (TX) and 7 (RX) are used
+        GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;                         // the pins are configured as alternate function so the USART peripheral has access to them
+        GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;                // this defines the IO speed and has nothing to do with the baud rate!
+        GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;                        // this defines the output type as push pull mode (as opposed to open drain)
+        GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;                        // this activates the pull up resistors on the IO pins
+        GPIO_Init(GPIOB, &GPIO_InitStruct);                                        // now all the values are passed to the GPIO_Init() function which sets the GPIO registers
+        
+        /* The RX and TX pins are now connected to their AF
+         * so that the USART1 can take over control of the 
+         * pins
+         */
+        GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_USART1);
+        GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
+        
+        /* Now the USART_InitStruct is used to define the 
+         * properties of USART1 
+         */
+        USART_InitStruct.USART_BaudRate = baudrate;                                  // the baud rate is set to the value we passed into this function
+        USART_InitStruct.USART_WordLength = USART_WordLength_8b;  // we want the data frame size to be 8 bits (standard)
+        USART_InitStruct.USART_StopBits = USART_StopBits_1;                  // we want 1 stop bit (standard)
+        USART_InitStruct.USART_Parity = USART_Parity_No;                  // we don't want a parity bit (standard)
+        USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // we don't want flow control (standard)
+        USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx; // we want to enable the transmitter and the receiver
+        USART_Init(USART1, &USART_InitStruct);                                          // again all the properties are passed to the USART_Init function which takes care of all the bit setting
+        
+        USART_Cmd(USART1, ENABLE);        //Enables USART1
+}
+
+void USART_puts(USART_TypeDef* USARTx, uint8_t data){
+		
+		// wait until data register is empty
+		while(!(USARTx->SR & 0x00000040)); 
+		USART_SendData(USARTx, data);
+}
 
 void Delay(__IO uint32_t nCount) {
   while(nCount--) {
@@ -50,8 +116,8 @@ uint16_t initialize_timers(uint16_t frequency, uint16_t preScaler)
 	 
 	// Compute prescaler value for timebase
 	uint16_t PrescalerValue = (uint16_t) ((SystemCoreClock /2) / (84000000 / preScaler)) - 1;  //To figure out what the numbers do
-	//second value in the divide is the frequency
-	uint16_t PreCalPeriod = ((84000000 / preScaler) / frequency) - 1;  //To figure out what the numbers do
+
+	uint16_t PreCalPeriod = ((84000000 / preScaler) / frequency) - 1; 
 
 	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;  //structure used by stm in initializing the pwm
     TIM_OCInitTypeDef  TIM_OCInitStructure;
@@ -102,11 +168,16 @@ void anologWrite(uint32_t channel, uint32_t period, uint8_t dutyCycle)
  *  to be at 0 degrees and 180 degrees respectively. 
  */
 
-void setAngle(uint32_t channel, uint8_t angle, uint16_t period)
-{
-	uint16_t length = 1000 * angle / 180 + 100;  //1000 is the length of the max - min pulse length in u S. 180 is the full range of angles the servo motor can move. 500 is the minimum pulse length that corresponds with 0 degrees.
+void setAngle(uint16_t channel, uint8_t angle, uint16_t period)
+{ 	
+	//uint16_t length = 1000 * angle / 135 + 100;  //1000 is the length of the max - min pulse length in u S. 180 is the full range of angles the servo motor can move. 
 	
-	TIM3->CCR1 = (period + 1) * 2.25 / 20;  //Sets the pulse width length to be length micro Seconds
+	TIM3->CCR1 = (((period + 1) / 20) * ((MAXSERVO - MINSERVO) * angle / MAXSERVOANGLE + MINSERVO ));
+	
+	//USART_puts(USART1, ((MAXSERVO - MINSERVO) * angle / MAXSERVOANGLE + MINSERVO) * 100);
+	USART_puts(USART1, ((period + 1) / 20) * (MAXSERVO - MINSERVO) * angle / MAXSERVOANGLE + MINSERVO);
+
+	//TIM3->CCR1 = (period + 1) / 20 * 1.5;  //Sets the pulse width length to be length milli Seconds
 }
 
 
@@ -119,21 +190,48 @@ int main(void)
 	uint32_t max = 2.25;  //max length of pulse in mili seconds that the servo will take
 	uint32_t min = 0.75;  //min length of pulse in mili seconds that the servo will take
 	
+	GPIO_InitTypeDef  GPIO_InitStructure;
+	
+	/* GPIOD Periph clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+  /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+  
+  init_USART1(115200);
+	
 	while(1)
 	{
-		TIM3->CCR1 = (period + 1) * 0.75 / 20;
-		
-		Delay(0xFFFFFF);
 		
 		
-		TIM3->CCR1 = (period + 1) * 1.5 / 20;
-		
-		Delay(0xFFFFFF);
+		setAngle(TIM3->CCR1, 0, period);
 		
 		
-		TIM3->CCR1 = (period + 1) * 2.25 / 20;
+		GPIO_SetBits(GPIOD, GPIO_Pin_12);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+		 
+		Delay(0x1FFFFFF);
 		
-		Delay(0xFFFFFF);
+		
+		setAngle(TIM3->CCR1, 70, period);
+		
+		GPIO_SetBits(GPIOD, GPIO_Pin_13);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+		
+		Delay(0x1FFFFFF);
+		
+		
+		setAngle(TIM3->CCR1, 130, period);
+		
+		GPIO_SetBits(GPIOD, GPIO_Pin_14);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+		
+		Delay(0x1FFFFFF); 
 	
 	}
 }
