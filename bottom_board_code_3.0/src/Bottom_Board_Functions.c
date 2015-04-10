3af6c003d87604240688609f877bb3d83010577c
LMcBee95 <lmcbee@purdue.edu>
2015-04-09 20:48:16 -0400

    Added time counting function to bottom board

--- bottom_board_code_3.0/src/Bottom_Board_Functions.c
+++ bottom_board_code_3.0/src/Bottom_Board_Functions.c
@@ -37,6 +37,8 @@ uint8_t twoPreviousValue = 0;  //The value from two serial readings ago
 uint8_t previousValue = 0;    //The value from the last serial reading
 uint8_t currentValue = 0;     //The current value from the serial
 
+/*** Time Function ***/
+uint32_t time = 0;  //Keeps track of the number of ms that the program has been running for (for time update look at TIM5_IRQHandler)
 
 GPIO_InitTypeDef  GPIO_InitStructure;  //this is used by all of the pin initiations, must be included
 
@@ -149,10 +151,6 @@ void RGBLedPwm(uint8_t dutyCycleRed, uint8_t dutyCycleGreen, uint8_t dutyCycleBl
 
 void sendDataUp(void)
 {
-<<<<<<< HEAD
-=======
-	//RGBLedPwm(255, 255, 255);
->>>>>>> origin/master
 	dataGoingUp[0] = START_BYTE;
 	dataGoingUp[SENT_PACKET_SIZE - 1] = END_BYTE;
 	dataGoingUp[SENT_PACKET_SIZE - 2] = checksum(dataGoingUp, SENT_PACKET_SIZE - 3);
@@ -194,6 +192,15 @@ void stepperPwm(uint8_t dutyCycle1, uint8_t dutyCycle2)
 {
 	TIM12->CCR1 = (GENERAL_PWM_PERIOD) * dutyCycle1 / 255.0;	
 	TIM12->CCR2 = (GENERAL_PWM_PERIOD) * dutyCycle2 / 255.0;	
+}
+
+void TIM5_IRQHandler(void)
+{
+ if (TIM_GetITStatus(TIM5, TIM_IT_Update) != RESET)
+ {
+	TIM_ClearITPendingBit(TIM5, TIM_IT_Update);
+	time++; //Updates the current time that the program has been running
+ }
 }
 
 void turnFootdPwm(uint8_t PWM_IN1, uint8_t PWM_IN2)
@@ -273,66 +280,11 @@ void USART1_IRQHandler(void) {
 }
 
 void USART2_IRQHandler(void) {
-    
-}
-
-void UART5_IRQHandler(void) {
-    
-	//Check if interrupt was because data is received
-	if (USART_GetITStatus(UART5, USART_IT_RXNE)) {
-		received = USART_ReceiveData(UART5);
-		
-		if(received == START_BYTE)
-		{
-			//RGBLedPwm(255, 255, 255);
-			pollStorage[pollCounter] = received;
-			pollCounter = 1;
-		}
-		else if(pollCounter > 0 && received != START_BYTE)
-		{
-			pollStorage[pollCounter] = received;
-			pollCounter++;
-			
-			if(pollCounter == MOTOR_PACKET_SIZE  && (checksum(pollStorage, MOTOR_PACKET_SIZE - 3) == pollStorage[MOTOR_PACKET_SIZE - 2]) && (pollStorage[MOTOR_PACKET_SIZE - 1] == END_BYTE))
-			{
-				//do stuff with the received data
-				
-				ORANGE_LED_ON
-				
-				pollCounter = 0; //Reset the counter
-				
-				
-				pollingMotors = 0;  //resets the variable that will allow the board to start sending out motor commands again
-				
-				GPIO_SetBits(USART6_ENABLE_PORT, USART6_ENABLE_PIN);  //sets the rs485 on the bottom board to read the response from polling the motors
-				GPIO_SetBits(USART6_DISABLE_PORT, USART6_DISABLE_PIN);
-				
-			}
-			else if(pollCounter == MOTOR_PACKET_SIZE)
-			{
-				//GPIO_ResetBits(GPIOD, GPIO_Pin_12);
-			}
-		}
-		else
-		{
-			pollCounter = 0;//Clear interrupt flag
-			USART_ClearITPendingBit(UART5, USART_IT_RXNE);
-		}
-	}
-        
-}
-
-void USART6_IRQHandler(void) {
-    
-	 //Check if interrupt was because data is received
-    if (USART_GetITStatus(USART6, USART_IT_RXNE)) 
+    //Check if interrupt was because data is received
+    if (USART_GetITStatus(USART2, USART_IT_RXNE)) 
 	{	
-<<<<<<< HEAD
 		received = USART_ReceiveData(USART2);
-=======
-		received = USART_ReceiveData(USART6);
 	
->>>>>>> origin/master
 		if(received == START_BYTE)
 		{
 			storage[counter] = received;
@@ -389,12 +341,171 @@ void USART6_IRQHandler(void) {
 				
 				if(!pollingMotors)  //if we are not polling the motors for fault data, pollingMotors will be 0 and the the code will send motor commands to the motor controllers
 				{
-<<<<<<< HEAD
+
 					
-=======
-					//RED_LED_OFF
-					//ORANGE_LED_OFF
->>>>>>> origin/master
+					sendPackets();	//Sends the motor controller commands produced by the convert function
+					pollCounter++;
+				
+					if(pollCounter > 20)
+					{
+						RED_LED_ON
+						pollMotor(pollAddress);
+						
+						pollingMotors = 1;
+						
+						Delay(0x3FFF);		//Wait for the read write pin to turn low
+						GPIO_ResetBits(USART6_ENABLE_PORT, USART6_ENABLE_PIN);  //sets the rs485 on the bottom board to read the response from polling the motors
+						GPIO_ResetBits(USART6_DISABLE_PORT, USART6_DISABLE_PIN);
+						
+						pollAddress++;
+						if(pollAddress == 9)
+						{
+							pollAddress = 1;
+						}
+						pollCounter = 0;  //Resets the poll counter
+
+					}
+				}
+				else
+				{
+					notPolledCounter++;
+					
+					if(notPolledCounter > POLL_MOTOR_TIME_OUT)
+					{
+						GPIO_SetBits(USART6_ENABLE_PORT, USART6_ENABLE_PIN);  //sets the rs485 on the bottom board to read the response from polling the motors
+						GPIO_SetBits(USART6_DISABLE_PORT, USART6_DISABLE_PIN);
+						pollingMotors = 0;
+						notPolledCounter = 0;
+						
+						//print error message or send some message to the top board.
+					}
+				}
+				counter = 0; //Reset the counter
+				
+			}
+			else if(counter == PACKET_SIZE)
+			{
+				
+			}
+		}
+		else
+		{
+			counter = 0;
+		}
+	}
+	USART_ClearITPendingBit(USART2, USART_IT_RXNE);
+}
+
+void UART5_IRQHandler(void) {
+    
+	//Check if interrupt was because data is received
+	if (USART_GetITStatus(UART5, USART_IT_RXNE)) {
+		received = USART_ReceiveData(UART5);
+		
+		if(received == START_BYTE)
+		{
+			//RGBLedPwm(255, 255, 255);
+			pollStorage[pollCounter] = received;
+			pollCounter = 1;
+		}
+		else if(pollCounter > 0 && received != START_BYTE)
+		{
+			pollStorage[pollCounter] = received;
+			pollCounter++;
+			
+			if(pollCounter == MOTOR_PACKET_SIZE  && (checksum(pollStorage, MOTOR_PACKET_SIZE - 3) == pollStorage[MOTOR_PACKET_SIZE - 2]) && (pollStorage[MOTOR_PACKET_SIZE - 1] == END_BYTE))
+			{
+				//do stuff with the received data
+				
+				ORANGE_LED_ON
+				
+				pollCounter = 0; //Reset the counter
+				
+				
+				pollingMotors = 0;  //resets the variable that will allow the board to start sending out motor commands again
+				
+				GPIO_SetBits(USART6_ENABLE_PORT, USART6_ENABLE_PIN);  //sets the rs485 on the bottom board to read the response from polling the motors
+				GPIO_SetBits(USART6_DISABLE_PORT, USART6_DISABLE_PIN);
+				
+			}
+			else if(pollCounter == MOTOR_PACKET_SIZE)
+			{
+				//GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+			}
+		}
+		else
+		{
+			pollCounter = 0;//Clear interrupt flag
+			USART_ClearITPendingBit(UART5, USART_IT_RXNE);
+		}
+	}
+        
+}
+
+void USART6_IRQHandler(void) {
+    
+	 //Check if interrupt was because data is received
+    if (USART_GetITStatus(USART6, USART_IT_RXNE)) 
+	{	
+		received = USART_ReceiveData(USART6);
+	
+		if(received == START_BYTE)
+		{
+			storage[counter] = received;
+			counter = 1;
+			
+		}
+		else if(counter > 0 && received != START_BYTE)
+		{
+			
+			storage[counter] = received;
+			counter++;
+			
+			if(counter == PACKET_SIZE  && (checksum(storage, PACKET_SIZE - 3) == storage[PACKET_SIZE - 2]) && (storage[PACKET_SIZE - 1] == END_BYTE))
+			{
+				RGBLedPwm(0, 255, 255);
+
+				sendDataUp();
+				convertTBtoBB(storage);  //Converts the data from the top board into motor controller commands that we can use
+				
+				/*** Do stuff with the info from the top board ***/
+				
+				
+				if(LED1_VALUE > 100)
+				{
+					RGBLedPwm(0, 255, 255);
+				}
+				
+				
+				cameraLedPwm(LED1_VALUE, LED2_VALUE, LED3_VALUE, LED4_VALUE, LED5_VALUE);
+				bilgePumpPwm(BILGE_PUMP_VALUE);
+				if(FOOT_TURNER_VALUE < 128) //Going Forward
+				{
+					uint8_t turnFootMag = (FOOT_TURNER_VALUE & 0x0F) << 1;
+					turnFootdPwm(turnFootMag, 0);
+				}
+				else //Going in Reverse
+				{
+					uint8_t turnFootMag = (FOOT_TURNER_VALUE & 0x0F) << 1;
+					turnFootdPwm(0, turnFootMag);
+				}
+				
+				if(READ_LASER) //Read in measurement for the laser tool
+				{
+					//Read in data from the laser measurement tool and figure out what the angle of
+					//the stepper is.  Also figure out the angle of the stepper motor. 
+				}
+				
+				if(READ_VOLTAGES)
+				{
+					//Figure out voltage flags
+				}
+				
+				/*** End doing stuff with the info from the top board ***/
+				
+				if(!pollingMotors)  //if we are not polling the motors for fault data, pollingMotors will be 0 and the the code will send motor commands to the motor controllers
+				{
+
 					
 					sendPackets();	//Sends the motor controller commands produced by the convert function
 					pollCounter++;
@@ -775,11 +886,18 @@ void init_IRQ(void)
 	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 3;  //sub priority assignment
 	NVIC_Init(&NVIC_InitStruct);
 	
-	//Initiate Interrupt Request for USART  channel 1
+	//Initiate Interrupt Request for UART  channel 5
 	NVIC_InitStruct.NVIC_IRQChannel = UART5_IRQn;  //sets the handler for USART1
 	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;  //sets the priority, or which interrupt will get called first if multiple interrupts go off at once. The lower the number, the higher the priority.
 	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 2;  //sub priority assignment
+	NVIC_Init(&NVIC_InitStruct);
+	
+    /* Enable the TIM3 gloabal Interrupt */
+	NVIC_InitStruct.NVIC_IRQChannel = TIM5_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 4;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStruct);
 }
 
@@ -1157,6 +1275,26 @@ void initialize_timer3(uint32_t frequency, uint16_t preScaler)
 	//return(PreCalPeriod);
 }
 
+void initialize_timer5(void)
+{
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;  //structure used by stm in initializing the pwm
+	
+	/* TIM5 clock enable */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+		
+	/* Time base configuration */
+	TIM_TimeBaseStructure.TIM_Period = 1000 - 1; //Frequency set to 1ms
+	TIM_TimeBaseStructure.TIM_Prescaler = 84 - 1;
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM5, &TIM_TimeBaseStructure);
+	 
+	/* TIM Interrupts enable */
+	TIM_ITConfig(TIM5, TIM_IT_Update, ENABLE);
+	/* TIM5 enable counter */
+	TIM_Cmd(TIM5, ENABLE);
+}
+
 void init_USART1(uint32_t baudrate){
         
         /* This is a concept that has to do with the libraries provided by ST
@@ -1381,4 +1519,3 @@ void init_USART6(uint32_t baudrate){
 	
 	USART_ITConfig(USART6, USART_IT_RXNE, ENABLE); // Enables Serial Interrupt
 }
-
