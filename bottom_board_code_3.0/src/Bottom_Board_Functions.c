3ca4b90062d2d9737850e34f5412c8e8270be9d2
rmcbee <rmcbee@purdue.edu>
2015-04-12 14:02:39 -0400

    update to program the micro board

--- bottom_board_code_3.0/src/Bottom_Board_Functions.c
+++ bottom_board_code_3.0/src/Bottom_Board_Functions.c
@@ -907,7 +907,7 @@ void init_LEDS(void)
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
 	/* Configures the LEDS*/
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11| GPIO_Pin_12| GPIO_Pin_13;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
