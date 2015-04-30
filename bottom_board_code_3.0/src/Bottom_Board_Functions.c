400561300471c3a7692464f45a8cd69191bc2bc8
Eric Colter <coltere@purdue.edu>
2015-04-30 19:57:10 -0400

    Fixed horrible timer bug from merge.

--- bottom_board_code_3.0/src/Bottom_Board_Functions.c
+++ bottom_board_code_3.0/src/Bottom_Board_Functions.c
@@ -214,21 +214,25 @@ void TIM5_IRQHandler(void)
  int i;
  if (TIM_GetITStatus(TIM5, TIM_IT_Update) != RESET)
  {
-	if(time%4000==2000)
-	{
-		RGBLedPwm(0,0,0);
-		setSteppersDebugByte(0xFF);
-		setSteppers();
-	}
-	else if(time%4000==0)
+	TIM_ClearITPendingBit(TIM5, TIM_IT_Update);
+	time++; //Updates the current time that the program has been running
+
+	if(time%2000==0 && time%4000==0)
 	{
 		RGBLedPwm(255,255,255);
+		setSteppersDebugByte(0xFF);
+		for(i=0;i<14; i++)
+			setSteppers();
+	}
+	if(time%2000==0 && time%4000!=0)
+	{
+		RGBLedPwm(0,0,0);
 		setSteppersDebugByte(0x77);
-		setSteppers();
+		for(i=0;i<14;i++)
+			setSteppers();
 	}
 	Stepper_Update(verticalStepper);
 	Stepper_Update(horizontalStepper);
-	time++;
  }
 
 }
