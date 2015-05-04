313bb8e9abd4cfa664819c637a1a695c854a0e0d
LMcBee95 <lmcbee@purdue.edu>
2015-05-04 18:32:18 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	bottom_board_code_3.0/src/Bottom_Board_Functions.c
    	bottom_board_code_3.0/src/main.c

--- bottom_board_code_3.0/src/main.c
+++ bottom_board_code_3.0/src/main.c
@@ -1,6 +1,6 @@
 
 
-/*									       Information about this code
+/*									       Information about this code!
  *				
  *	This code currently takes a packet from the top board, converts it to packets the motor controllers can use, and
  *	then sends the packets out to the motors. The code will automatically poll a motor every twenty packets
@@ -10,7 +10,7 @@
  *	the Rx is A3 and the Tx is A2.	
  *
  *  The important part of the code is at the bottom, within main(). Everything before that is just initializing
- *  variables and creating functions.
+ *  variables and creating functions
  *
  */
 
@@ -23,7 +23,7 @@ int main(void) {
 	init_DMA_ADC1(ADC1ConvertedValue, NUM_DMA_ADC1_CONVERSIONS); //the function does not actually use the array address yet
 	//init_DMA_ADC3(ADC3ConvertedValue, NUM_DMA_ADC3_CONVERSIONS);  //the function does not actually use the array address yet
 	init_IRQ();
-	init_LEDS();
+	
 	init_RGB_led_timers(100000, 1);
 	
 	initialize_servo_timer();
@@ -31,6 +31,9 @@ int main(void) {
 	init_USART2(TOP_BOTTOM_BAUD);	// initialize USART2 baud rate
 	init_UART5(BOTTOM_MOTOR_BAUD);	// initialize USART2 baud rate
 	init_USART6(TOP_BOTTOM_BAUD); 	// initialize USART6 baud rate
+	
+	initialize_claw1_timer(100000 , 1);
+	initialize_claw2_timer(100000 , 1);
 	
 	initialize_stepper_objects();
 	initialize_stepper_pins();
@@ -41,6 +44,7 @@ int main(void) {
 	//initialize_stepper_timer(100000, 1);
 	initialize_led_timers(LED_PWM_FREQUENCY, 1);
 	initialize_timer3(100000, 1);
+	//initialize_timer5();
 	
 
 	GPIO_SetBits(USART6_ENABLE_PORT, USART6_ENABLE_PIN);  //sets the rs485 on the bottom board to read the response from polling the motors
@@ -49,19 +53,28 @@ int main(void) {
 	Delay(0xFFF); //Delays to give the read/write pin time to initialize
 	
 	RGBLedPwm(0, 0, 0);
-	//initialize_timer5();
-	
+	 init_LEDS();
 	while (1)
 	{
 		cameraLedPwm(128, 128, 128, 128, 128);
 		GPIO_SetBits(GPIOD, GPIO_Pin_10);
 		
-		Delay(0xffffff);
-	
-		GPIO_ResetBits(GPIOD, GPIO_Pin_10);
 		
 		Delay(0xffffff);
 		
+		GPIO_ResetBits(GPIOD, GPIO_Pin_13 );
+		GPIO_ResetBits(GPIOD, GPIO_Pin_12 );
+		GPIO_ResetBits(GPIOD, GPIO_Pin_11 );
+		GPIO_ResetBits(GPIOD, GPIO_Pin_10 );
+		
+		clawPwm(175, 0);
+		//turnFootdPwm(175, 0);
+		setServo1Angle(120);
+		setServo2Angle(120);
+		bilgePumpPwm(100);
+		
+		
+		Delay(0xffffff);
 	}
 	
 	return(0);
